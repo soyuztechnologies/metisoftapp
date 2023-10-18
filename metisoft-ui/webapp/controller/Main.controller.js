eda4867af90bb9fb476eb7d03482d1bf8cb834a8
Vaishali Tyagi <vaishali@evotrainingsolutions.com>
2023-10-18 04:16:00 +0000

    final changes

--- metisoft-ui/webapp/controller/Main.controller.js
+++ metisoft-ui/webapp/controller/Main.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("com.meti.metisoftui.controller.Main", {
@@ -64,6 +65,67 @@ sap.ui.define([
                 //     }
 
                 // });
+            },
+            onClick2: function(){
+                var that = this;
+                var myPayload = {
+                    "CtLibri":[{
+                        "item":{
+                            "Mandt":"?",
+                            "CodLibro":"?",
+                            "Title":"?",
+                            "Autore":"?",
+                            "LettoSi":"?",
+                            "CasaEdi":"?"
+                        }
+                    }],
+                    "ICreate":"?",
+                     "IDelete":"?",
+                      "IGet":"?",
+                       "IGetall":"X",
+                        "IUpdt":"?"
+                };
+                $.ajax({
+                    url: '/IntegrationSuiteDestination/http/test-api/',
+                    type: 'POST',
+                    headers:{
+                        "Content-type": "application/json"
+                    },
+                    dataType: 'json',
+                    async: true,
+                    data : JSON.stringify(myPayload),
+                    success: function(data)
+                    {
+                        that.oModel.setProperty("/item",data["n0:ZBcSonialibriRfcResponse"].CtLibri.item);
+                    },
+                    error: function(err, status){
+                        debugger;
+                    }
+                });
+            },
+            onClick3: function(){
+                var that = this;
+                var myPayload = {
+                    "CtLibri":[{
+                        "item":{
+                            "Mandt":"?",
+                            "CodLibro":"?",
+                            "Title":"?",
+                            "Autore":"?",
+                            "LettoSi":"?",
+                            "CasaEdi":"?"
+                        }
+                    }],
+                    "ICreate":"?",
+                     "IDelete":"?",
+                      "IGet":"?",
+                       "IGetall":"X",
+                        "IUpdt":"?"
+                };
+                var oLocal = new JSONModel();
+                oLocal.loadData("/http/test-api/", myPayload, true, "POST", false, false)
+                .then(function(data){debugger;})
+                .catch(function(error){debugger;})
             }
         });
     });
